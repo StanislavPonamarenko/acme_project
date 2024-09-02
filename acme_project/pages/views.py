653b2096e86b78ae21cbcb17a5c9d7c39920ec33
Станислав Понамаренко <stanislav.pop.18@gmail.com>
2024-09-02 14:22:03 +0300

    02.09_14.21

--- acme_project/pages/views.py
+++ acme_project/pages/views.py
@@ -5,3 +5,12 @@ from birthday.models import Birthday
 
 class HomePage(TemplateView):
     template_name = 'pages/index.html'
+
+    def get_context_data(self, **kwargs):
+        # Получаем словарь контекста из родительского метода.
+        context = super().get_context_data(**kwargs)
+        # Добавляем в словарь ключ total_count;
+        # значение ключа — число объектов модели Birthday.
+        context['total_count'] = Birthday.objects.count()
+        # Возвращаем изменённый словарь контекста.
+        return context
