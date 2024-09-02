3c78ff357bc9e92ce13d3f94d484bc230e11f903
Станислав Понамаренко <stanislav.pop.18@gmail.com>
2024-09-02 17:36:28 +0300

    02.09_17.36

--- acme_project/acme_project/settings.py
+++ acme_project/acme_project/settings.py
@@ -8,6 +8,14 @@ DEBUG = True
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+
+EMAIL_FILE_PATH = BASE_DIR / 'sent_emails'
+
+LOGIN_REDIRECT_URL = 'pages:homepage'
+
+LOGIN_URL = 'login'
+
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
